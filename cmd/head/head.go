47c1eec431e12ace748a4efafc623502f6ee87ba
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-03 17:11:55 +0300

    refactor(head): provide a better error handling logic

--- cmd/head/head.go
+++ cmd/head/head.go
@@ -4,52 +4,50 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
-func parseFlagAndArgument() (int, string, error) {
-	var numOfLines int
-	flag.IntVar(&numOfLines, "n", 10, "number of lines")
+func parseFlagAndArgument() (uint, string, error) {
+	var numOfLines uint
+	flag.UintVar(&numOfLines, "n", 10, "number of lines")
 	flag.Parse()
 
 	filePath := flag.Arg(0)
 
-	var ErrInvalidFlag = fmt.Errorf("invalid flag, you have to specify a positive integer number of lines")
-	var ErrInvalidFilePath = fmt.Errorf("invalid file path, you have to specify a valid path to text file")
-
-	if numOfLines <= 0 {
-		return 0, "", ErrInvalidFlag
-	}
-
 	if filePath == "" {
-		return 0, "", ErrInvalidFilePath
+		return 0, "", fmt.Errorf("invalid file path, you have to specify a valid path to text file")
 	}
 
 	return numOfLines, filePath, nil
 }
 
-func printFileLines(filePath string, numOfLines int) {
+func printFileLines(filePath string, numOfLines uint) error {
 	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println(err)
+		return fmt.Errorf("cannot open this file for reading: No such file or directory")
 	}
 
 	scanner := bufio.NewScanner(file)
 
-	lineCounter := 0
+	var lineCounter uint = 0
 	for scanner.Scan() && lineCounter < numOfLines {
 		line := scanner.Text()
 		lineCounter++
 		fmt.Println(line)
 	}
+	return nil
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 	numOfLines, filePath, err := parseFlagAndArgument()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	printFileLines(filePath, numOfLines)
-
+	handleError(err)
+	err = printFileLines(filePath, numOfLines)
+	handleError(err)
 }
