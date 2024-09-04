b7c4cd1f8a1042553b3bf32bb53d9931379ff48f
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-04 16:28:06 +0300

    feat(cat): implement core functionality of the cat command with the options of line numbering

--- cmd/cat/cat.go
+++ cmd/cat/cat.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 )
 
 func parseFlagsAndArguments() (bool, []string, error) {
@@ -21,8 +23,38 @@ func parseFlagsAndArguments() (bool, []string, error) {
 	return lineNumberingFlag, filesPaths, nil
 }
 
+func cat(lineNumberingFlag bool, filesPaths []string) error {
+	var lineCounter uint = 1
+
+	for _, path := range filesPaths {
+		file, err := os.Open(path)
+		if err != nil {
+			return fmt.Errorf("%s: cannot open this file for reading: No such file or directory", path)
+		}
+
+		scanner := bufio.NewScanner(file)
+
+		if lineNumberingFlag {
+			for scanner.Scan() {
+				fmt.Println(lineCounter, "	", scanner.Text())
+				lineCounter++
+			}
+		} else {
+			for scanner.Scan() {
+				fmt.Println(scanner.Text())
+			}
+		}
+	}
+
+	return nil
+}
+
 func main() {
-	_, _, err := parseFlagsAndArguments()
+	lineNumberingFlag, filesPaths, err := parseFlagsAndArguments()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cat(lineNumberingFlag, filesPaths)
 	if err != nil {
 		log.Fatal(err)
 	}
