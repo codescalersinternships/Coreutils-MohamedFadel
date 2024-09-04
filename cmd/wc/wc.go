1df0784ddc815e78dc0227d94331caa8497af15e
Mohamed Fadel <mohamedfadelpro1@gmail.com>
2024-09-04 13:16:57 +0300

    feat(wc): add count function to handle line, word, and character counts

--- cmd/wc/wc.go
+++ cmd/wc/wc.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"os"
+
+	utils "github.com/codescalersinternships/Coreutils-MohamedFadel/internal"
 )
 
 func countLines(scanner *bufio.Scanner) {
@@ -38,6 +41,52 @@ func countCharacters(scanner *bufio.Scanner) {
 
 }
 
+func count(filePath string, lFlag, wFlag, cFlag bool) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("cannot open this file for reading: No such file or directory")
+	}
+	defer file.Close()
+
+	if lFlag {
+		file.Seek(0, 0)
+		scanner := bufio.NewScanner(file)
+		countLines(scanner)
+	}
+
+	if wFlag {
+		file.Seek(0, 0)
+		scanner := bufio.NewScanner(file)
+		countWords(scanner)
+	}
+
+	if cFlag {
+		file.Seek(0, 0)
+		scanner := bufio.NewScanner(file)
+		countCharacters(scanner)
+	}
+
+	if !lFlag && !wFlag && !cFlag {
+		scanner := bufio.NewScanner(file)
+		file.Seek(0, 0)
+		countLines(scanner)
+
+		file.Seek(0, 0)
+		scanner = bufio.NewScanner(file)
+		countWords(scanner)
+
+		file.Seek(0, 0)
+		scanner = bufio.NewScanner(file)
+		countCharacters(scanner)
+	}
+
+	return nil
+}
+
 func main() {
+	_, filePath, lFlag, wFlag, cFlag, err := utils.ParseFlagsAndArgument()
+	utils.HandleError(err)
+	err = count(filePath, lFlag, wFlag, cFlag)
+	utils.HandleError(err)
 
 }
